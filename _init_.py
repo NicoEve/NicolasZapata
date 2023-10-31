0876793c68cb786157bbdcaaad160b40dba27596
NicoEve <nz579701@gmail.com>
2023-10-30 19:18:34 -0500

    Potencia

--- _init_.py
+++ _init_.py
@@ -1,24 +1,26 @@
-from operations import divide
+from operations import power
 
 def game():
     score = 0
     while True:
         print('======== Menu ========'
-            '\n1. Divide'
+            '\n1. Power'
             '\n0. Exit')
         option = int(input('\nChoose an option: '))
         if option == 0:
             break
-        num_1 = int(input('Enter first number: '))
-        num_2 = int(input('Enter second number: '))
-        answer = float(input('Enter your answer: '))
+        num_1 = int(input('Enter base number: '))
+        num_2 = int(input('Enter exponent number: '))
+        answer = int(input('Enter your answer: '))
         if option == 1:
-            result = divide(num_1, num_2)
-            if result is not None and result == answer:
-                score += 2  
+            result = power(num_1, num_2)
+            if result == answer:
+                score += 4  
                 print('Correct!!')
             else:
                 print('Incorrect')
     print(f'======== Game Over ========'
         f'\nYour score is {score}'
         '\nKeep going!')
+
+game()
